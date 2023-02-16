42219fed0d7441e958d0ef029531ac8d69708b72
Rishat Murzyev <rmurzyev@mail.ru>
2023-02-16 16:47:21 +0500

    Updated with sourcePath

---
+++ CreatePlugin.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using PhoneApp.Domain.DTO;
+using PhoneApp.Domain.Interfaces;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace EmployeesCreatePlugin
+{
+    public class CreatePlugin
+    {
+        private const string jsonFilePath = "Resources.resx";
+        public List<EmployeesDTO> _employees { get; set; }
+        public string SaveInfoLog(List<EmployeesDTO> log)
+        {
+            return WriteJson(jsonFilePath, log);
+        }
+        private string WriteJson(string filePath, List<EmployeesDTO> log)
+        {
+            string json = JsonConvert.SerializeObject(log);
+
+            using (StreamWriter sw = new StreamWriter(filePath, true))
+            {
+                sw.Write(json);
+            }
+            return json;
+        }
+    }
+}
