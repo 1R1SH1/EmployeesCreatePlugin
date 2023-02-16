35aad80c4c66cc40fba867d24a891036a1345f1c
Rishat Murzyev <rmurzyev@mail.ru>
2023-02-16 17:15:42 +0500

    Updated

--- CreatePlugin.cs
+++ CreatePlugin.cs
@@ -7,10 +7,20 @@ using System.Linq;
 
 namespace EmployeesCreatePlugin
 {
-    public class CreatePlugin
+    public class Plugin : IPluggable
     {
+        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
         private const string jsonFilePath = "Resources.resx";
         public List<EmployeesDTO> _employees { get; set; }
+        public IEnumerable<DataTransferObject> Run(IEnumerable<DataTransferObject> args)
+        {
+            logger.Info("Loading employees");
+
+            var employeesList = SaveInfoLog(_employees);
+
+            logger.Info($"Loaded {employeesList.Count()} employees");
+            return employeesList.Cast<DataTransferObject>();
+        }        
         public string SaveInfoLog(List<EmployeesDTO> log)
         {
             return WriteJson(jsonFilePath, log);
